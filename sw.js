577f1425f67d025fb893551a9c6dab84844fab8a
Quân Phạm Ngọc <ngocquanofficial@gmail.com>
2024-02-01 02:56:08 +0700

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706719199';
+const cacheName = 'chirpy-1706719556';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
