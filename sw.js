c44e59d2f1333182acdd2eadb76677fdc83e87b0
Quân Phạm Ngọc <ngocquanofficial@gmail.com>
2024-02-06 23:35:07 +0700

    part 1, definition

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707064297';
+const cacheName = 'chirpy-1707236895';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
