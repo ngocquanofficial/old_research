2bea43a3d15b5085bbcf11857b9dfe371da6ccda
Quân Phạm Ngọc <ngocquanofficial@gmail.com>
2024-02-09 08:28:43 +0700

    pretty avt

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707372312';
+const cacheName = 'chirpy-1707442066';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
