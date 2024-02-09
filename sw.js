e2bae0abdab31d30eeae1062969f36449b7bca02
Quân Phạm Ngọc <ngocquanofficial@gmail.com>
2024-02-09 16:09:44 +0700

    end of the year

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707442066';
+const cacheName = 'chirpy-1707468784';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
